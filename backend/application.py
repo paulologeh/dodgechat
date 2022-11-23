de4bfff517c5f0eff5e4ea527be22fed193980fd
Paul Ologeh <paulologeh@protonmail.com>
2022-11-23 00:03:13 +0000

    add setup-database cli command
    added new command to help quickly setup database on a new environment

--- backend/application.py
+++ backend/application.py
@@ -1,5 +1,5 @@
-import logging
 import os
+import logging
 import socket
 
 from dotenv import load_dotenv
@@ -10,17 +10,15 @@ from app.models import *
 
 load_dotenv()
 
-LOG_LEVEL = int(os.getenv("LOG_LEVEL", 10))
+LOG_LEVEL = int(os.environ.get("LOG_LEVEL", 20))
 
 logging.basicConfig(
-    filename="flask.log",
+    filename=os.environ.get("LOG_FILE_NAME", "flask.log"),
     level=LOG_LEVEL,
     format="%(asctime)s - [%(container_id)s] [%(levelname)s] %(name)s "
-    "[%(module)s.%(funcName)s:%(lineno)d]: %("
-    "message)s",
-    datefmt="%Y%m%d-%H:%M:%S",
+    "[%(module)s.%(funcName)s:%(lineno)d]: %(message)s",
+    datefmt="%Y%m%d-%H-:%M:%S",
 )
-
 
 old_factory = logging.getLogRecordFactory()
 
@@ -33,12 +31,30 @@ def record_factory(*args, **kwargs):
 
 logging.setLogRecordFactory(record_factory)
 
-app = create_app(os.getenv("ENVIRONMENT"))
+
+werkzeug_logger = logging.getLogger("werkzeug")
+werkzeug_logger.level = LOG_LEVEL
+
+app = create_app(os.getenv("ENVIRONMENT", "DEVELOPMENT"))
 migrate = Migrate(app, db)
+
 
 for logger in (
     app.logger,
+    logging.getLogger("sqlalchemy"),
     logging.getLogger("werkzeug"),
     logging.getLogger("flask_cors"),
 ):
     logger.setLevel(LOG_LEVEL)
+
+
+@app.cli.command("setup-database")
+def setup_db():
+    try:
+        db.drop_all()
+        print("Setting up database...")
+        db.create_all()
+        print("Database setup successfully")
+    except Exception as e:
+        print("Failed to setup database")
+        print(str(e))
