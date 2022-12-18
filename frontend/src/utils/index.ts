21abb445e242573278b0907ebb637f645b0eff01
Paul Ologeh <paulologeh@protonmail.com>
2022-12-18 22:32:50 +0000

    improve last seen calculation

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -77,13 +77,18 @@ export const getLastSeen = (date: Date) => {
   const now = new Date()
   const diff = now.getTime() - datetime.getTime()
   if (diff <= 600000) {
-    // 10 minutes
+    // Less than 10 minutes
     return 'Now'
+  } else if (diff <= 3600000) {
+    // Less than 1 hour
+    const minutes = Math.round(diff / 60000)
+    return `${minutes} minutes ago`
+  } else if (diff === 3600000) {
+    // equal to 1 hour
+    return '1 hour ago'
   } else if (diff <= 86400000) {
-    const time = Math.round(diff / 3600000)
-    if (time === 1) {
-      return `${time} hour ago`
-    }
+    // less than a day
+    const time = Math.floor(diff / 3600000)
     return `${time} hours ago`
   } else {
     return 'Over a day ago'
