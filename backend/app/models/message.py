9eca1520f93759e6611b1ef700aa77dc21134164
Paul Ologeh <paulologeh@protonmail.com>
2022-12-20 12:32:19 +0000

    fix message relationship

--- backend/app/models/message.py
+++ backend/app/models/message.py
@@ -15,7 +15,7 @@ class Message(db.Model):
     conversation_id = db.Column(
         UUID(as_uuid=True), db.ForeignKey("conversations.id", ondelete="CASCADE")
     )
-    conversation = db.relationship("Conversation", back_populates="conversations")
+    conversation = db.relationship("Conversation", back_populates="message")
     body = db.Column(db.Text(), nullable=False)
     read = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow, index=True)
