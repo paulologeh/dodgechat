771945425ffc83868f4b66f94ea42c27d8460be8
Paul Ologeh <paulologeh@protonmail.com>
2022-12-31 04:54:56 +0000

    add eventlet and use stable gunicorn

--- backend/gunicorn.config.py
+++ backend/gunicorn.config.py
@@ -1,16 +1,10 @@
-import multiprocessing
-import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 bind = "0.0.0.0:8000"
-workers = (
-    multiprocessing.cpu_count() * 2 + 1
-    if os.environ.get("ENVIRONMENT") == "PRODUCTION"
-    else 1
-)
+worker_class = "eventlet"
+workers = 1
 accesslog = "flask.log"
 errorlog = "flask.log"
 capture_output = True
