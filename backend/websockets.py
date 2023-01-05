6b266a301eaf2149d460b4480bf268a0732c13b0
Paul Ologeh <paulologeh@protonmail.com>
2023-01-05 11:04:32 +0000

    send events to active clients only

--- backend/websockets.py
+++ backend/websockets.py
@@ -1,17 +1,27 @@
 import logging
 from app import socketio
 from flask_login import current_user
+from flask import request
 
 logger = logging.getLogger(__name__)
 
+ACTIVE_CLIENTS = {}
 
-def conversation_changed_event(data):
-    socketio.emit("message", {"data": data}, namespace="/")
+
+def send_event_to_client(data, user_id):
+    client_sid = ACTIVE_CLIENTS.get(user_id)
+    # send update to client only if it is connected
+    if client_sid:
+        try:
+            socketio.emit("message", {"data": data}, namespace="/", to=client_sid)
+        except Exception as e:
+            logger.exception("Failed to notify client of update %s" % e)
 
 
 @socketio.event
 def connect():
     if current_user.is_authenticated:
+        ACTIVE_CLIENTS.update({current_user.id: request.sid})
         logger.info("Client connected - id:%s" % current_user.id)
     else:
         return False
@@ -20,6 +30,7 @@ def connect():
 @socketio.event()
 def disconnect():
     if getattr(current_user, "id", None) is not None:
+        ACTIVE_CLIENTS.pop(current_user.id)
         logger.info("Client disconnected - id:%s" % current_user.id)
     else:
         logger.warning("Client disconnected - id:Unknown")
