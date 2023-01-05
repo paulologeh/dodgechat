fc0017cbcabd034cce47a3c102048717f679bd3c
Paul Ologeh <paulologeh@protonmail.com>
2023-01-05 15:27:58 +0000

    only remove active clients that exist

--- backend/websockets.py
+++ backend/websockets.py
@@ -10,7 +10,7 @@ ACTIVE_CLIENTS = {}
 
 def send_event_to_client(data, user_id):
     client_sid = ACTIVE_CLIENTS.get(user_id)
-    # send update to client only if it is connected
+    # send update to client only if it is active
     if client_sid:
         try:
             socketio.emit("message", {"data": data}, namespace="/", to=client_sid)
@@ -30,7 +30,8 @@ def connect():
 @socketio.event()
 def disconnect():
     if getattr(current_user, "id", None) is not None:
-        ACTIVE_CLIENTS.pop(current_user.id)
+        if current_user.id in ACTIVE_CLIENTS:
+            ACTIVE_CLIENTS.pop(current_user.id)
         logger.info("Client disconnected - id:%s" % current_user.id)
     else:
         logger.warning("Client disconnected - id:Unknown")
