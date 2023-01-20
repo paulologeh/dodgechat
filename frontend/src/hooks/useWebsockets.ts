b5ef1909103ddf1458bb95368e1add14f55b5627
Paul Ologeh <paulologeh@protonmail.com>
2023-01-20 20:28:31 +0000

    update build - FE

--- frontend/src/hooks/useWebsockets.ts
+++ frontend/src/hooks/useWebsockets.ts
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
 import { SocketEvent } from 'types/sockets'
 import { useUser } from 'contexts/userContext'
-import beepSound from 'assets/messagealert.mp3'
 
 const SERVER = process.env.APP_URL ?? ''
 const socket = io(SERVER, { autoConnect: false, withCredentials: true })
@@ -27,8 +26,6 @@ export const useWebsockets = () => {
     })
 
     socket.on('message', (payload) => {
-      const audio = new Audio(beepSound)
-      audio.play().catch(console.error)
       const { data } = payload
       const newevents = [...events, data]
       setEvents(newevents)
